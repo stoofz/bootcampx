6c08a41d7356fd40b6674946f2c0e171e75cd4a3
Anthony Chambers <ajfchambers@gmail.com>
2023-04-27 10:11:06 -0400

    Add parameterize queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,21 +7,30 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+  FROM assistance_requests
+  JOIN students ON assistance_requests.student_id = students.id
+  JOIN teachers ON assistance_requests.teacher_id = teachers.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name ASC
+  LIMIT $2;
+  `;
 
-SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
-FROM assistance_requests
-JOIN students ON assistance_requests.student_id = students.id
-JOIN teachers ON assistance_requests.teacher_id = teachers.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-ORDER BY teachers.name ASC
-
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort_name}: ${user.teacher_name}`);
     });
   })
   .catch(err => console.error('query error', err.stack));
+
+
+
+
